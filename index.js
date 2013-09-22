47bec7ccf67b8e3cdd273c47a891668e2581e634
Alfio Zappala <azappala@azaptree.com>
2013-09-22 09:24:23 -0400

    0.0.12 release - added event listener for SIGTERM to trigger clean shutdown

--- index.js
+++ index.js
@@ -17,20 +17,63 @@
 (function() {
 	'use strict';
 
+	var domain = require('domain');
+	var serverDomain = domain.create();
+
 	var Hapi = require('hapi');
 
 	var manifest = require('./manifest');
 	var composer = new Hapi.Composer(manifest);
 
-	composer.compose(function(err) {
-		if (err) {
-			console.error('Failed composing servers : ' + err.message);
-		} else {
-			console.log('Hapi is composed.');
-			composer.start(function() {
-				console.log('All servers started');
+	var log = function(msg) {
+		console.log(new Date().toISOString() + ' : ' + msg);
+	};
+
+	var logError = function(msg) {
+		console.error(new Date().toISOString() + ' : ' + msg);
+	};
+
+	var shutdown = function(event) {
+		log(event);
+		if (composer) {
+			log('Shutting down ...');
+			composer.stop({
+				timeout : 60 * 1000
+			}, function() {
+				composer = undefined;
+				log('All servers stopped');
+				serverDomain.dispose();
+				process.exit(0);
 			});
 		}
+	};
+
+	process.on('exit', function() {
+		shutdown('exit');
 	});
-	
+
+	process.on('SIGTERM', function() {
+		shutdown('SIGTERM');
+	});
+
+	serverDomain.on('error', function(error) {
+		logError('Unexpected error - process will exit : ' + error);
+		shutdown();
+		process.exit(1);
+	});
+
+	serverDomain.run(function() {
+		composer.compose(function(err) {
+			if (err) {
+				logError('Failed composing servers : ' + err.message);
+				throw err;
+			} else {
+				log('Hapi is composed.');
+				composer.start(function() {
+					log('All servers started : pid = ' + process.pid);
+				});
+			}
+		});
+	});
+
 }());
